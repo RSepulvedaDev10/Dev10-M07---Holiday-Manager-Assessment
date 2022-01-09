4b8cb1736cfbcaabdfd1dd8bd534cff18caa97e9
RSepulvedaDev10 <94391613+RSepulvedaDev10@users.noreply.github.com>
2022-01-08 21:57:00 -0600

    Update TestFile.py

--- TestFile.py
+++ TestFile.py
@@ -1,16 +1,14 @@
 import requests
-weatherURL = 'https://rapidapi.com/community/api/open-weather-map'
 
-queryString = {'q': 'Milwaukee, WI', 'lat': '43.0389', 'lon': '-87.9065', 'cnt': '7', 'units': 'imperial'}
+url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
+
+querystring = {"q":"Milwaukee, WI","lat":"43.0389","lon":"-87.9065","cnt":"7","units":"imperial"}
 
 headers = {
-    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-    'x-rapidapi-key': '09a0e25236msh6ada605176faf8bp1b2d9fjsn3c2e43a1e536'
-}
+    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+    'x-rapidapi-key': "09a0e25236msh6ada605176faf8bp1b2d9fjsn3c2e43a1e536"
+    }
 
-weather = requests.request("GET", weatherURL, headers=headers, params=queryString)
-weatherDict = weather.json()
+response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(weatherDict)
-
-print(weatherDict['list'][0]['weather'][0]['main'])
+print(response.text)
